cb03a0bc42a90ec4a976ecbe5523acc5c506941d
Sham <shamgamage@purplecrowlidar.ca>
2017-12-21 14:08:02 -0500

    asr codes were fixed

--- asrSham.m
+++ asrSham.m
@@ -47,7 +47,7 @@ lambda_em = 354.7;
 %% These from BOB- WVOEM.m
 in.LRfree = 50; % was 20 on 0305, 0308 50, 200905-6 50
 in.LRpbl = 80; % 50 on 0305; was 80 on otherwise
-in.LRtranHeight = 2000; % this is the height to the BL
+in.LRtranHeight = 1800; % this is the height to the BL
 % 3 is nominal, not accurate 2.75; 
 
 
