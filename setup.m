9387727c3edbf13efebbd3ddcb861fd9578accac
Sham <shamgamage@purplecrowlidar.ca>
2018-08-29 11:54:01 -0400

    doesnt run, asr issue

---
+++ setup.m
@@ -0,0 +1,22 @@
+function config=setup(file)
+
+% open file
+fid=fopen(file);
+
+% read first line
+t=fgetl(fid);
+while isempty(t)==1
+    t=fgetl(fid);
+end
+
+while t~=-1
+
+    eval(t);
+    t=fgetl(fid);
+    while isempty(t)==1
+        t=fgetl(fid);
+    end
+    
+end
+
+fclose(fid);
